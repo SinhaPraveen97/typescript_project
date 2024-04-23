3abf9cab6558aada61de7207119440eac30cc62f
SinhaPraveen97 <praveensinha97@gmail.com>
2024-04-24 00:39:38 +0530

    another commit with types example

---
+++ Types/variables.js
@@ -0,0 +1,7 @@
+var movieTitle = "Tiger";
+movieTitle = "Kanha";
+//movieTitle = 9;
+var numCatLives = 9;
+numCatLives += 1;
+var gameOver = false;
+gameOver = true;
---
+++ Types/variables.ts
@@ -0,0 +1,10 @@
+let movieTitle :string ="Tiger";
+movieTitle ="Kanha";
+//movieTitle = 9;
+
+let numCatLives: number = 9;
+numCatLives +=1;
+
+let gameOver : boolean = false;
+gameOver = true;
+
