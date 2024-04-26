5675ee79b420a1dda9c85fbabb67cb378c2923a2
SinhaPraveen97 <praveensinha97@gmail.com>
2024-04-27 00:04:27 +0530

    function parameter types

---
+++ Types/functionParameterTypes.js
@@ -0,0 +1,6 @@
+function square(num) {
+    console.log(num * num);
+}
+square(3);
+//square("praveen");
+//square(true);
---
+++ Types/functionParameterTypes.ts
@@ -0,0 +1,9 @@
+function square(num : number)
+{
+    console.log(num*num);
+}
+
+square(3);
+//square("praveen");
+//square(true);
+
